3124f6cc1db6280b3dbe86467b252433ed1ad909
Austin Vosler <austinpvosler@gmail.com>
2025-12-15 23:18:12 -0500

    Update app.routes.ts

--- BetterPlacemaking.CLIENT/src/app/app.routes.ts
+++ BetterPlacemaking.CLIENT/src/app/app.routes.ts
@@ -1,11 +1,33 @@
 import { Routes } from '@angular/router';
 import { DefaultLayout } from './layouts/default-layout/default-layout';
+import { Login } from './views/login/login';
+
+const admin: Routes = [
+  { path: '', data: { subtree: 'admin-root' }, children: [] },
+  { path: 'users', data: { subtree: 'admin-users' }, children: [] },
+  { path: 'settings', data: { subtree: 'admin-settings' }, children: [] },
+];
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: DefaultLayout,
+  { path: 'login', component: Login },
+
+  {
+    path: '',
+    component: DefaultLayout,
+    children: [
+      { path: 'admin', children: admin },
+
+      {
+        path: 'project/:projectId',
         children: [
-        ]
-    }
+          { path: '', },
+          { path: 'overview', },
+          { path: 'tasks', },
+          { path: 'settings', },
+
+          { path: 'admin', children: admin },
+        ],
+      },
+    ],
+  },
 ];
