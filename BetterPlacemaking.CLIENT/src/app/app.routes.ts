fb1df8f8c2d05f3ad66863f55571e3a5bc05f6a9
Austin Vosler <austinpvosler@gmail.com>
2025-12-15 23:37:09 -0500

    fix for routes not compiling #3

--- BetterPlacemaking.CLIENT/src/app/app.routes.ts
+++ BetterPlacemaking.CLIENT/src/app/app.routes.ts
@@ -3,9 +3,9 @@ import { DefaultLayout } from './layouts/default-layout/default-layout';
 import { Login } from './views/login/login';
 
 const admin: Routes = [
-  { path: '', data: { subtree: 'admin-root' }, children: [] },
-  { path: 'users', data: { subtree: 'admin-users' }, children: [] },
-  { path: 'settings', data: { subtree: 'admin-settings' }, children: [] },
+  // { path: '', data: { subtree: 'admin-root' }, children: [] },
+  // { path: 'users', data: { subtree: 'admin-users' }, children: [] },
+  // { path: 'settings', data: { subtree: 'admin-settings' }, children: [] },
 ];
 
 export const routes: Routes = [
@@ -15,19 +15,19 @@ export const routes: Routes = [
     path: '',
     component: DefaultLayout,
     children: [
-      { path: 'admin', children: admin },
+      // { path: 'admin', children: admin },
 
-      {
-        path: 'project/:projectId',
-        children: [
-          { path: '', },
-          { path: 'overview', },
-          { path: 'tasks', },
-          { path: 'settings', },
+      // {
+      //   path: 'project/:projectId',
+      //   children: [
+      //     { path: '', },
+      //     { path: 'overview', },
+      //     { path: 'tasks', },
+      //     { path: 'settings', },
 
-          { path: 'admin', children: admin },
-        ],
-      },
+      //     { path: 'admin', children: admin },
+      //   ],
+      // },
     ],
   },
 ];
